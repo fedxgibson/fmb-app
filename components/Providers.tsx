e3b1290e088cd94e98ec2a7766210ac7c1d82226
Federico Martinez <fed.x.gibson@gmail.com>
2024-11-02 20:22:11 -0300

    wip

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { ThemeProvider } from 'next-themes'
-import { motion, AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
